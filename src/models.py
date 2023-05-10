8755356995d37978642b4ff77954e863fc0e4b65
Isaac Sanchez <saacman27@gmail.com>
2023-05-10 23:54:10 +0200

    added resnet model

--- src/models.py
+++ src/models.py
@@ -15,3 +15,64 @@ class MLP(nn.Module):
         # x = self.soft(self.linears[-1](x))
         x = self.linears[-1](x)
         return x
+    
+class ResNet(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(ResNet, self).__init__()
+        self.in_channels = in_channels
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(64, 2)
+        self.layer2 = self._make_layer(128, 2, stride=2)
+        self.layer3 = self._make_layer(256, 2, stride=2)
+        self.layer4 = self._make_layer(512, 2, stride=2)
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(self, out_channels, num_blocks, stride=1):
+        layers = []
+        layers.append(ResidualBlock(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels
+        for _ in range(num_blocks-1):
+            layers.append(ResidualBlock(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = nn.ReLU()(x)
+        x = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avg_pool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        identity = x
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = nn.ReLU()(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x += self.shortcut(identity)
+        x = nn.ReLU()(x)
+        return x
