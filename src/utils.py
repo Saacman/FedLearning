d1bd96ec88740fa8afc7b756b49eaad5a422582a
Isaac Sanchez <saacman27@gmail.com>
2023-06-16 10:40:22 +0200

    Check models notebook working

--- src/utils.py
+++ src/utils.py
@@ -166,23 +166,23 @@ def evaluate_model(model, test_loader, device, criterion=None):
 
     running_loss = 0
     running_corrects = 0
+    with torch.no_grad():
+        for inputs, labels in test_loader:
 
-    for inputs, labels in test_loader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
 
-        inputs = inputs.to(device)
-        labels = labels.to(device)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
 
-        outputs = model(inputs)
-        _, preds = torch.max(outputs, 1)
+            if criterion is not None:
+                loss = criterion(outputs, labels).item()
+            else:
+                loss = 0
 
-        if criterion is not None:
-            loss = criterion(outputs, labels).item()
-        else:
-            loss = 0
-
-        # statistics
-        running_loss += loss * inputs.size(0)
-        running_corrects += torch.sum(preds == labels.data)
+            # statistics
+            running_loss += loss * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
 
     eval_loss = running_loss / len(test_loader.dataset)
     eval_accuracy = running_corrects / len(test_loader.dataset)
